2d0808fdc73bca20c1fbf9d434c1e7e9d677d352
Milena28-dev <62910143+Milena28-dev@users.noreply.github.com>
2020-07-07 17:56:06 -0500

    componente shop

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../../page/Home';
+import Shop from '../../components/shop/Shop';
 
 const Router = () => (
 
     <BrowserRouter>
         <Switch >
             <Route exact path="/" component={ Home }/>
+            <Route exact path="/Shop" component={ Shop }/>
         </Switch>
     </BrowserRouter>
 )
