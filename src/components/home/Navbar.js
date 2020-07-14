55147d697574848f1108dcea5557942f2eb78c87
Eligreg Lopez <eligregl@gmail.com>
2020-07-14 17:31:38 -0500

    cambios a los links de la barra de navegación

--- src/components/home/Navbar.js
+++ src/components/home/Navbar.js
@@ -1,126 +1,39 @@
-import React from 'react'
+import React from "react";
 import "../../styles/Navbar.scss";
 // import logo from '../../components/img/logo.png';
-import {Link} from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 // import '../../styles/slider.scss';
-
-
 
 const Navbar = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-dark  ">
-        <a className="navbar-brand text-light" href="#" to='/'>
-          Booky
-      </a>
-      {/* <image src={logo}/> */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon "></span>
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="/">Booky</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <div
-          className="collapse navbar-collapse " id="navbarSupportedContent">
-          {/* className='nav'> */}
-          <ul className="navbar-nav mr-auto ">
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle text-light"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Home
-            </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
-                  Action
-              </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-              </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">
-                  Something else here
-              </a>
-              </div>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle text-light"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Description
-            </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
-                  Action
-              </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-              </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">
-                  Something else here
-              </a>
-              </div>
+            <li class="nav-item">
+              <a class="nav-link" href="/Description">Description</a>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle text-light"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="/Shop" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Shop
-            </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
-                  Action
               </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-              </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">
-                  Something else here
-              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/Checkout">Checkout</a>
               </div>
             </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control btn-outline-secondary mr-sm-2 "
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0 text-light "
-              id="buton"
-              type="submit"
-            >
-              Search
-          </button>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
         </div>
       </nav>
